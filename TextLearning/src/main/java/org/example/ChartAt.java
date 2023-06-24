931c4f4e2a1884beeadb499200be351a3039a522
levanquoc <levanquocbkdn@gmail.com>
2023-06-24 20:54:12 +0700

    chartAt()

---
+++ TextLearning/src/main/java/org/example/ChartAt.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ChartAt {
+    public static void main(String[] args) {
+        String myName ="Quoc";
+        int length = myName.length();
+        if(length>= 3){
+            System.out.println(myName.charAt(3)); // c
+        }
+
+    }
+}
